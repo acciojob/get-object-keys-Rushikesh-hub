8f6b32169fccd28f5975e06f800c300a9ec45b1b
Rushikesh Muneshwar <77239940+Rushikesh-hub@users.noreply.github.com>
2026-07-03 11:03:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-const student = {
-  name: "John"
-};
-
-function getKeys(obj) {
-  return Object.keys(obj);
-}
-
 console.log(getKeys(student));
+console.log(getKeys({ name: "John", age: 20, city: "New York" }));
